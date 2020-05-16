3311d27f5dd9abede0ce3fef333b7d9649da8fcc
Mohamed Magdy Kamal <56923348+mpd2019@users.noreply.github.com>
2020-05-16 23:56:16 +0200

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,11 @@
+var pas1 = "lamiaa",
+    pas2 = "LAMIAA",
+    pas3 = "Lamiaa",
+    pas11 = "aya",
+    pas22 = "AYA",
+    pas33 = "Aya",
+    message = "لا يسمح لك بالدخول",
+   ooo = prompt ( message );
+if(ooo != pas1 && ooo != pas2 && ooo != pas3 && ooo != pas11 && ooo != pas22 && ooo != pas33) {
+location.href ="/"
+}
