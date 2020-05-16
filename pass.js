26f1456ee87fae45bb8f9a5d2cb5faebad41b9d5
Mohamed Magdy Kamal <56923348+mpd2019@users.noreply.github.com>
2020-05-17 00:02:09 +0200

    Update pass.js

--- pass.js
+++ pass.js
@@ -1,9 +1,9 @@
-var pas1 = "lamiaa",
-    pas2 = "LAMIAA",
-    pas3 = "Lamiaa",
-    pas11 = "aya",
-    pas22 = "AYA",
-    pas33 = "Aya",
+var pas1 = "\u006c\u0061\u006d\u0069\u0061\u0061",
+    pas2 = "\u004c\u0041\u004d\u0049\u0041\u0041",
+    pas3 = "\u004c\u0061\u006d\u0069\u0061\u0061",
+    pas11 = "\u0061\u0079\u0061",
+    pas22 = "\u0041\u0059\u0041",
+    pas33 = "\u0041\u0079\u0061",
     message = "لا يسمح لك بالدخول",
    ooo = prompt ( message );
 if(ooo != pas1 && ooo != pas2 && ooo != pas3 && ooo != pas11 && ooo != pas22 && ooo != pas33) {
